a03a3cc7ac8dd452e49ab626ddd83e6e13e174bf
asargues <asarte82@gmail.com>
2025-02-13 16:05:40 +0100

    Add RED test to check remove

--- spec/features/lps_spec.rb
+++ spec/features/lps_spec.rb
@@ -43,27 +43,54 @@ RSpec.describe 'LP Management', type: :system do
   end
 
   context 'with nested songs', :js do
-    it 'allows creating a new LP with nested songs' do
+    before do
       driven_by(:selenium_chrome_headless)
+    end
 
+    def create_lp_with_songs(lp_name:, description:, songs:)
       visit lps_path
       click_on 'New LP'
-      fill_in 'LP Name *', with: 'Let It Be'
-      fill_in 'Description', with: 'Final album'
+      fill_in 'LP Name *', with: lp_name
+      fill_in 'Description', with: description
       select 'The Beatles', from: 'Artist'
-
-      click_on 'Add Song'
-
-      using_wait_time(10) do
-        within all('.nested-field').last do
-          find("input[placeholder='Enter song name']", visible: :all).set('Across the Universe')
+      songs.each do |song_name|
+        click_on 'Add Song'
+        using_wait_time(10) do
+          within(all('.nested-field').last) do
+            find("input[placeholder='Enter song name']", visible: :all).set(song_name)
+          end
         end
       end
-
       click_on 'Create'
+    end
 
+    def remove_nested_song(song_name)
+      click_on 'Edit'
+      field = all('.nested-field').find do |nested_field|
+        nested_field.find("input[placeholder='Enter song name']").value == song_name
+      end
+      within(field) do
+        click_on 'Remove'
+      end
+      click_on 'Update'
+    end
+
+    it 'allows creating a new LP with nested songs' do
+      create_lp_with_songs(lp_name: 'Let It Be', description: 'Final album', songs: ['Across the Universe'])
       expect(page).to have_content('Let It Be')
       expect(page).to have_content('Across the Universe')
+    end
+
+    it 'allows deleting a nested song from an LP' do
+      create_lp_with_songs(lp_name: 'Let It Be', description: 'Final album', songs: ['Song One', 'Song Two'])
+      expect(page).to have_content('Let It Be')
+      expect(page).to have_content('Song One')
+      expect(page).to have_content('Song Two')
+
+      remove_nested_song('Song One')
+      expect(page).to have_content('Let It Be')
+      expect(page).to have_no_content('Song One')
+      expect(page).to have_content('Song Two')
     end
   end
 end
