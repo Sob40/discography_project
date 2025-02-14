e2e332af65611d1932121173b7d2e496fdbc226b
asargues <asarte82@gmail.com>
2025-02-14 15:17:12 +0100

    Add slim gem

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+eagerLoadControllersFrom("controllers", application)
+window.Stimulus = application
 
 export { application }
